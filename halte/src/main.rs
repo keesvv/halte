f7fd5120fc63bc40a7ef0475e487d3cbee45dab7
Kees van Voorthuizen <me@keesvv.nl>
2023-05-05 15:14:17 +0200

    feat: add HTTP/stdin interfacing, Stop & Pass struct

--- halte/src/main.rs
+++ halte/src/main.rs
@@ -1,8 +1,27 @@
-use clap::Parser;
+use std::io::{self, Read};
 
-#[derive(Parser)]
-struct Args;
+use clap::Parser;
+use openov::TimingPointResponse;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Timing point code
+    #[arg(short)]
+    tpc: u32,
+    /// OVAPI endpoint
+    #[arg(long, default_value = "https://v0.ovapi.nl")]
+    endpoint: String,
+}
 
 fn main() {
-    Args::parse();
+    // let args = Args::parse();
+    // let response = reqwest::blocking::get(format!("{}/tpc/{}", args.endpoint, args.tpc)).unwrap();
+    // let dec = response.json::<TimingPointResponse>().unwrap();
+
+    // TODO: remove, this is just for debugging purposes
+    let mut data = Vec::new();
+    io::stdin().read_to_end(&mut data).unwrap();
+
+    let dec = serde_json::from_slice::<TimingPointResponse>(&data).unwrap();
+    println!("{:#?}", dec);
 }
--- openov/src/lib.rs
+++ openov/src/lib.rs
@@ -1,17 +1,53 @@
-pub struct TimingPointCode {}
+use std::collections::HashMap;
 
+use serde::Deserialize;
+
+pub type TimingPoints = HashMap<String, TimingPoint>;
+
+pub type Passes = HashMap<String, Pass>;
+
+#[derive(Deserialize, Debug)]
+pub struct TimingPointResponse {
+    #[serde(flatten)]
+    pub tps: TimingPoints,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TimingPoint {
+    #[serde(rename = "Stop")]
+    pub stop: Stop,
+    #[serde(rename = "Passes")]
+    pub passes: Passes,
+}
+
+#[derive(Deserialize, Debug)]
 pub enum Accessibility {
+    #[serde(rename = "ACCESSIBLE")]
     Accessible,
 }
 
-pub struct Position(f32, f32);
-
+#[derive(Deserialize, Debug)]
 pub struct Stop {
-    pub position: Position,
+    #[serde(rename = "Latitude")]
+    pub lat: f32,
+    #[serde(rename = "Longitude")]
+    pub long: f32,
+    #[serde(rename = "TimingPointTown")]
     pub town: String,
+    #[serde(rename = "TimingPointName")]
     pub tpc_name: String,
-    pub tpc_code: u32,
-    pub stop_area_code: u16,
+    #[serde(rename = "TimingPointCode")]
+    pub tpc_code: String,
+    #[serde(rename = "StopAreaCode")]
+    pub stop_area_code: String,
+    #[serde(rename = "TimingPointWheelChairAccessible")]
     pub tp_wheelchair: Accessibility,
+    #[serde(rename = "TimingPointVisualAccessible")]
     pub tp_visual: Accessibility,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Pass {
+    #[serde(rename = "DestinationName50")]
+    pub destination_name: String,
 }
