852cb2cbcdee43930be791771f2c019cae02b9cd
Kees van Voorthuizen <me@keesvv.nl>
2023-05-08 00:06:47 +0200

    feat: implement TryFrom & multiple data sources

--- halte/src/main.rs
+++ halte/src/main.rs
@@ -3,10 +3,20 @@ use std::io::{self, Read};
 use halte::Stop;
 use openov::tp::TimingPointResponse;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+
+#[derive(ValueEnum, Debug, Clone, Copy)]
+enum Source {
+    HTTP,
+    #[clap(alias = "-")]
+    Stdin,
+}
 
 #[derive(Parser, Debug)]
 struct Args {
+    /// Data source
+    #[arg(short, default_value = "http")]
+    source: Source,
     /// Timing point code
     #[arg(short)]
     tpc: u32,
@@ -15,15 +25,29 @@ struct Args {
     endpoint: String,
 }
 
+impl TryFrom<Args> for TimingPointResponse {
+    // TODO: handle errors
+    type Error = ();
+
+    fn try_from(args: Args) -> Result<Self, Self::Error> {
+        Ok(match args.source {
+            Source::HTTP => {
+                let response =
+                    reqwest::blocking::get(format!("{}/tpc/{}", args.endpoint, args.tpc)).unwrap();
+                response.json().unwrap()
+            }
+            Source::Stdin => {
+                let mut data = Vec::new();
+                io::stdin().read_to_end(&mut data).unwrap();
+                serde_json::from_slice(&data).unwrap()
+            }
+        })
+    }
+}
+
 fn main() {
-    // let args = Args::parse();
-    // let response = reqwest::blocking::get(format!("{}/tpc/{}", args.endpoint, args.tpc)).unwrap();
-    // let dec = response.json::<TimingPointResponse>().unwrap();
+    let args = Args::parse();
+    let response = TimingPointResponse::try_from(args).unwrap();
 
-    // TODO: remove, this is just for debugging purposes
-    let mut data = Vec::new();
-    io::stdin().read_to_end(&mut data).unwrap();
-
-    let dec = serde_json::from_slice::<TimingPointResponse>(&data).unwrap();
-    println!("{:#?}", Stop::from(dec));
+    println!("{:#?}", Stop::from(response));
 }
