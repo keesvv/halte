445ba8778305e597f3bb90311765927bc831f7a9
Kees van Voorthuizen <me@keesvv.nl>
2023-05-08 16:39:11 +0200

    feat: add Swift boilerplate

---
+++ halte-swift/Halte/HalteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalteApp.swift
+//  Halte
+//
+//  Created by Kees van Voorthuizen on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HalteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ halte-swift/HalteUITests/HalteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HalteUITestsLaunchTests.swift
+//  HalteUITests
+//
+//  Created by Kees van Voorthuizen on 08/05/2023.
+//
+
+import XCTest
+
+final class HalteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
